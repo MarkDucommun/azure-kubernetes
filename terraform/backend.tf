db5922782396b074f02f46dbdf5ff3e0b6977f03
Mark Ducommun <ducommun.mark@gmail.com>
2024-11-15 12:20:09 -0600

    better backend auth?

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,9 @@ terraform {
     storage_account_name  = "markducommuntfstate"      # Storage account name
     container_name        = "tfstate"                  # Container for state files
     key                   = "k8s-cluster.terraform.tfstate"  # Unique key for this state file
+    use_oidc              = true
+    client_id = var.client_id
+    tenant_id = var.tenant_id
+    subscription_id = var.subscription_id
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "=4.1.0"
     }
   }
   required_version = ">= 1.0.0"
@@ -11,7 +11,4 @@ terraform {
 provider "azurerm" {
   features {}
   use_oidc = true
-  tenant_id = var.tenant_id
-  client_id = var.client_id
-  subscription_id = var.subscription_id
 }
