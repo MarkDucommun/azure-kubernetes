009462134663b3fc801db441ee03d8596d4871a4
Mark Ducommun <ducommun.mark@gmail.com>
2024-12-06 15:49:06 -0600

    Try again?

--- terraform_prerequisites/main.tf
+++ terraform_prerequisites/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "k8s_rg" {
 }
 
 resource "azurerm_storage_account" "k8s_packer_storage_account" {
-  name                     = var.packer_storage_account_name
+  name                     = "packerstorageaccount"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
