993666ae49ab8061b0ae4d1604162a8a9843fcf9
Mark Ducommun <ducommun.mark@gmail.com>
2024-12-06 15:55:27 -0600

    Proper dependencies

--- terraform_prerequisites/main.tf
+++ terraform_prerequisites/main.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "k8s_rg" {
 
 resource "azurerm_storage_account" "k8s_packer_storage_account" {
   name                     = "packerstorageaccount"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
+  resource_group_name      = azurerm_resource_group.k8s_rg.name
+  location                 = azurerm_resource_group.k8s_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
